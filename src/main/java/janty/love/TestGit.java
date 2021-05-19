55e90f0391a2be2caffa9296c85d4650a76be35a
janty <jantypan@163.com>
2021-05-19 21:34:46 +0800

    First Commit

---
+++ src/main/java/janty/love/TestGit.java
@@ -0,0 +1,15 @@
+package janty.love;
+
+/******************************
+ * @Time ： 2021/5/19: 21:28
+ * @Author : Janty_Pan
+ * @Description :
+ *******************************
+ *
+ */
+
+public class TestGit {
+    public static void main(String[] args) {
+        System.out.println("Hello Git & Idea");
+    }
+}
